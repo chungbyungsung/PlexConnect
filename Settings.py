aa6dbaa7b5ea567a9cbfe11acb16295bbfe55450
chungbyungsung <a1125@chol.com>
2013-08-05 16:53:06 +0900

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -21,7 +21,7 @@ g_settings = { \
     'port_pms'        :('32400',), \
     \
     'enable_dnsserver':('True', 'False'), \
-    'ip_dnsmaster'    :('8.8.8.8',), \
+    'ip_dnsmaster'    :('192.168.0.21',), \
     \
     'enable_plexconnect_autodetect':('True', 'False'), \
     'ip_plexconnect'  :('0.0.0.0',), \
